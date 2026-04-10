abed930c427edb86f5b8e350f1ca6f577baa4644
vothuckhanhhuyen <66659535+vothuckhanhhuyen@users.noreply.github.com>
2026-04-10 08:36:00 +0000

    Deploying to gh-pages from @ vothuckhanhhuyen/huyenvo.github.io@ef06875ea6e88fa5c311aa6cccb0d7951b1196e9 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://huyenvo.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://vothuckhanhhuyen.github.io/#contact">contact us</a>.'
               }
             ]
           }
